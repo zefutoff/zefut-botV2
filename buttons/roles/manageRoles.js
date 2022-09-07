e123a051852aaebe466a515a9936a40e69f4a9ce
zefut <zefut.pro@gmail.com>
2022-09-07 23:07:12 +0200

    Correction des messages Log

--- buttons/roles/manageRoles.js
+++ buttons/roles/manageRoles.js
@@ -1,36 +1,33 @@
-//AddWarn
-const { Interaction } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require('discord.js');
 const { log } = require('../../utils/channels.json');
-
-const admin = require('firebase-admin');
-let db = admin.firestore();
+const { youtube, twitch, Mcreator } = require('../../utils/roles.json');
 
 module.exports = {
     name: 'manageRoles',
     async execute(client, interaction) {
+        const logs = new MessageEmbed().setColor('RED').setTitle('').setTimestamp().setFooter({ text: interaction.member.user.username });
+
+        const response = new MessageEmbed().setColor('GREEN');
+
         interaction.reply({
-            embeds: [response.setDescription(`✅ ${user} **a été warn pour la raison suivante** : ${reason} \n **Nombre de warn : 3**`)],
             components: [
                 new MessageActionRow().addComponents(
                     new MessageSelectMenu()
-                        .setCustomId(`rolesList, ${interaction.member.user.id}, ${interaction.member.user}`)
+                        .setMaxValues(3)
+                        .setCustomId(`manageRoles, ${interaction.member.user.id}, ${interaction.member.user}`)
                         .setPlaceholder('Aucun sujet selectionné')
                         .addOptions([
                             {
-                                label: "Contestation d'une sanction",
-                                value: 'sanction_report'
+                                label: 'YouTube',
+                                value: youtube
                             },
                             {
-                                label: 'Rapporter un bug',
-                                value: 'bug_report'
+                                label: 'Twitch',
+                                value: twitch
                             },
                             {
-                                label: 'Problème avec un modérateur',
-                                value: 'moderator_report'
-                            },
-                            {
-                                label: 'Problème avec un administrateur',
-                                value: 'administrator_report'
+                                label: 'Mcreator',
+                                value: Mcreator
                             }
                         ])
                 )
@@ -39,7 +36,7 @@ module.exports = {
         });
 
         interaction.guild.channels.cache.get(log).send({
-            embeds: [logs.setTitle('Gérer roles').setDescription(`${user} a utilisé la commande de modification des roles`)]
+            embeds: [logs.setTitle('Gérer roles').setDescription(`a utilisé la commande de modification des roles`)]
         });
     }
 };
