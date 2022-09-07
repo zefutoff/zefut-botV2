19af183ccba3ad0478c67707196b58e67ed0b457
zefut <zefut.pro@gmail.com>
2022-09-07 23:08:12 +0200

    Deplacement du fichier dans un sous dossier

--- events/message/messageCreate.js
+++ events/message/messageCreate.js
@@ -16,15 +16,17 @@ module.exports = {
         const logs = new MessageEmbed().setColor('RED').setTitle('[Auto-mod] Message supprimé').setTimestamp();
         const response = new MessageEmbed().setColor('GREEN');
 
+        const user = message.member.user;
+        const userName = user.username;
+        const userId = user.id;
+
         if (message.author.bot) return;
 
         if (message.mentions.members.size > 0) {
             message.guild.channels.cache.get(log).send({
                 embeds: [
                     logs
-                        .setDescription(
-                            `**Message contenant la mention :** ${message.content} \n \n **De :**${message.member.user.username} \n  \n **Dans le channel :**${message.channel}`
-                        )
+                        .setDescription(`**Message contenant la mention :** ${message.content} \n \n **De :**${userName} \n  \n **Dans le channel :**${message.channel}`)
                         .setTitle('[Log-mention]')
                 ]
             });
@@ -39,23 +41,41 @@ module.exports = {
                 if (message.content.includes(bannedVersions[index].version)) {
                     if (bannedVersions[index].exlued === '%message%' && manageMessage() == true) return;
                     message.reply({
-                        embeds: [response.setDescription(bannedVersions[index].response.replaceAll('%username%', message.member.user))],
+                        embeds: [response.setDescription(bannedVersions[index].response.replaceAll('%username%', user))],
                         components: [
                             new MessageActionRow().addComponents(
                                 new MessageButton().setLabel('Comment mettre à jour mon mod ?').setStyle('LINK').setURL('https://www.youtube.com/watch?v=Ut-POeZ-V3U'),
                                 new MessageButton().setLabel('Dernière version de Mcreator').setStyle('LINK').setURL('https://mcreator.net/download')
                             )
-                        ]
+                        ],
+                        ephemeral: true
                     });
 
-                    message.guild.channels.cache.get(log).send({
+                    return message.guild.channels.cache.get(log).send({
                         embeds: [
-                            logs.setDescription(
-                                `**Message suprimmé :** ${message.content} \n \n **De :**${message.member.user.username} \n  \n **Dans le channel :**${message.channel}`
-                            )
+                            logs.setDescription(`**Message suprimmé :** ${message.content} \n \n **De :**${userName} \n  \n **Dans le channel :**${message.channel}`)
                         ]
                     });
                 }
+            }
+
+            if (message.content.length > 20) {
+                await message.channel.threads.create({
+                    name: message.content.substring(0, 99).replace(/\n|\r/g, '')
+                });
+
+                const thread = message.channel.threads.cache.find(async x => (await x.name) === message.content.substring(0, 99));
+
+                await thread.members.add(userId);
+
+                message.guild.channels.cache.get(log).send({
+                    embeds: [
+                        //Message de log a refaire entirement
+                        logs.setDescription(
+                            `**Fils Mcreator :** ${message.content.substring(0, 99)} \n \n **De :**${userName} \n  \n **Message enovyé :**${message.content}`
+                        )
+                    ]
+                });
             }
         }
 
@@ -65,14 +85,10 @@ module.exports = {
                 if (bannedWords[index].exlued === '%ban%' && banMember() == true) return;
                 if (bannedWords[index].exlued === '%message%' && manageMessage() == true) return;
 
-                message.guild.channels.cache.get(message.channelId).send(bannedWords[index].response.replaceAll('%username%', message.member.user));
+                message.guild.channels.cache.get(message.channelId).send(bannedWords[index].response.replaceAll('%username%', user));
 
                 message.guild.channels.cache.get(log).send({
-                    embeds: [
-                        logs.setDescription(
-                            `**Message suprimmé :** ${message.content} \n \n **De :**${message.member.user.username} \n  \n **Dans le channel :**${message.channel}`
-                        )
-                    ]
+                    embeds: [logs.setDescription(`**Message suprimmé :** ${message.content} \n \n **De :**${userName} \n  \n **Dans le channel :**${message.channel}`)]
                 });
 
                 if (bannedWords[index].delete === true) message.delete();
